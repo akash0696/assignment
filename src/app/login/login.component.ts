c786dfd50e0eead1c61f9de66dfc4feeb8eb0ad1
Akash Kumar Mehta <akmehta064@gmail.com>
2021-04-10 08:00:25 +0530

    login authentication successful

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -12,13 +12,28 @@ export class LoginComponent implements OnInit {
   form: FormGroup;
   constructor(private fb: FormBuilder, private auth: AuthService) {
     this.form = this.fb.group({
-      email: ['', Validators.required],
-      password: ['', Validators.required]
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]]
   });
   }
 
   ngOnInit(): void {
 
+  }
+
+  loginUser(): void{
+    if (this.form.valid) {
+      this.auth.loginUser(this.form.value).subscribe(result => {
+        if (result.is_success){
+          console.log(result);
+          // alert(result.is_success);
+        }
+        // else{
+        //   alert(result.error.message);
+        //   console.log(result);
+        // }
+      });
+    }
   }
 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { baseUrl } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private loginUrl = 'https://demo.credy.in/api/v1/usermodule/login/';
   constructor(private http: HttpClient) {}
 
-
+  loginUser(data: any): Observable<any>{
+    // console.log('I am server');
+    return this.http.post<any>(`${baseUrl}v1/usermodule/login/`, data);
+    }
 }
